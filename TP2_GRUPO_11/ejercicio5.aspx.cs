d1666c14311013c1e61e24eb2d58463b483c8d3a
Tomas <tomas.a.gallardo006@gmail.com>
2025-04-11 19:42:15 -0300

    Productos y gigas

--- TP2_GRUPO_11/ejercicio5.aspx.cs
+++ TP2_GRUPO_11/ejercicio5.aspx.cs
@@ -17,8 +17,18 @@ namespace TP2_GRUPO_11
         protected void btnPrecioCalculado(object sender, EventArgs e)
         {
             string valorSeleccionado = DropDownListMemoria.SelectedValue;
+            decimal precioG = Convert.ToDecimal(valorSeleccionado);
+            decimal totalP = 0;
 
-            txtPrecioFinal.Text = $"El precio final es de: ${valorSeleccionado}";
+            foreach (ListItem item in checkBoxLstAccesorios.Items)
+            {
+                if (item.Selected)
+                {
+                    totalP += Convert.ToDecimal(item.Value);
+                }
+            }
+            decimal precioF = precioG + totalP;
+            txtPrecioFinal.Text = $"El precio final es de: ${precioF}"; 
         }
     }
 }
